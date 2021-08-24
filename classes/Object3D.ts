a628504d57e62a856650c169d0d5a320f897d2e7
Wellinator <wellcoj@gmail.com>
2021-08-24 20:30:03 -0300

    2 files modified

--- classes/Object3D.ts
+++ classes/Object3D.ts
@@ -10,9 +10,7 @@ abstract class Object3D {
   abstract faces: number[][];
   public constants: ConstantsService;
 
-  constructor(
-    size: number
-  ) {
+  constructor(size: number) {
     this.size = size;
     this.constants = new ConstantsService();
   }
@@ -54,29 +52,24 @@ abstract class Object3D {
   }
 
   public project3DPoint(point: Point3D): Point2D {
-    const factor =
-      this.constants.fieldOfView / (this.constants.VIEW_DISTANCE + point.Z);
-    this.constants.PerspectiveMatrix.forEach(row => {
-      point.X += point.X * row[0];
-      point.Y += point.Y * row[1];
-      point.Z += point.Z * row[2];
-    });
-    point.X = point.X * factor + this.constants.WIDTH / 2;
-    point.Y = point.Y * factor + this.constants.HEIGHT / 2;
-    return new Point2D(point.X, point.Y);
+    const factor = this.constants.fieldOfView / (this.constants.VIEW_DISTANCE + point.Z);
+    return new Point2D(
+      point.X * factor + this.constants.WIDTH / 2,
+      point.Y * factor + this.constants.HEIGHT / 2
+    );
   }
 
   public render(context: CanvasRenderingContext2D) {
     let currentFace = 0;
     this.faces.forEach((face, index) => {
-      let _p1 = this.mesh[face[0]];
-      let _p2 = this.mesh[face[1]];
-      let _p3 = this.mesh[face[2]];
+      const _p1 = this.mesh[face[0]];
+      const _p2 = this.mesh[face[1]];
+      const _p3 = this.mesh[face[2]];
 
-      let _v1 = new Point3D(_p2.X - _p1.X, _p2.Y - _p1.Y, _p2.Z - _p1.Z);
-      let _v2 = new Point3D(_p3.X - _p1.X, _p3.Y - _p1.Y, _p3.Z - _p1.Z);
+      const _v1 = new Point3D(_p2.X - _p1.X, _p2.Y - _p1.Y, _p2.Z - _p1.Z);
+      const _v2 = new Point3D(_p3.X - _p1.X, _p3.Y - _p1.Y, _p3.Z - _p1.Z);
 
-      let n = new Point3D(
+      const n = new Point3D(
         _v1.Y * _v2.Z - _v1.Z * _v2.Y,
         _v1.Z * _v2.X - _v1.X * _v2.Z,
         _v1.X * _v2.Y - _v1.Y * _v2.X
