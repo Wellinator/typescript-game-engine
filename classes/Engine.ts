6dd7093f6d6decf91965e195a9f48691a2f62469
Wellinator <wellcoj@gmail.com>
2021-09-01 16:53:11 -0300

    Create Engine class

---
+++ classes/Engine.ts
@@ -0,0 +1,44 @@
+import { ConstantsService } from '../services/constants.service';
+
+export class Engine {
+  private fpsTimes: number[] = [];
+  private context: CanvasRenderingContext2D;
+  private constantsService: ConstantsService = new ConstantsService();
+
+  constructor(
+    canvas: HTMLCanvasElement,
+    WIDTH: number = document.documentElement.clientWidth,
+    HEIGHT: number = document.documentElement.clientHeight
+  ) {
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    this.context = canvas.getContext('2d');
+    this.context.font = "10px Courier New";
+    this.context.lineWidth = this.constantsService.PIXEL_SIZE;
+    this.context.strokeStyle = 'white';
+    this.gameLoop()
+  }
+
+  private gameLoop() {
+    this.context.fillStyle = 'black';
+    this.context.fillRect(0, 0, this.constantsService.WIDTH, this.constantsService.WIDTH);
+    
+    if(this.constantsService.DEBUG_MODE){
+      const FPS = this.fpsCounter();
+      this.context.fillStyle = '#FFF';
+      this.context.fillText(`FPS: ${FPS.toFixed(2)}`, this.constantsService.WIDTH - 100, 20);
+    }
+    window.requestAnimationFrame(this.gameLoop);
+  }
+
+  private fpsCounter(): number {
+    let fps = performance.now();
+    const now = performance.now();
+    while (this.fpsTimes.length > 0 && this.fpsTimes[0] <= fps - 1000) {
+      this.fpsTimes.shift();
+    }
+    this.fpsTimes.push(now);
+    return this.fpsTimes.length;
+  }
+
+}
--- index.ts
+++ index.ts
@@ -1,23 +1,15 @@
 import { Cube } from './classes/Cube';
+import { Engine } from './classes/Engine';
 import { ConstantsService } from './services/constants.service';
 
 const constantService = new ConstantsService();
-
 const canvas: HTMLCanvasElement = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = constantService.WIDTH;
-canvas.height = constantService.HEIGHT;
 
-
-ctx.font = "10px Courier New";
-ctx.lineWidth = constantService.PIXEL_SIZE;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeStyle = 'white';
-
+const engine = new Engine(canvas);
 const cube = new Cube( 0, 0, 400, 100);
+//cube.render();
 
 window.document.addEventListener('keydown', (event: KeyboardEvent)=> queryButtons(event))
-
 const queryButtons = (event: KeyboardEvent): void => {
   event.preventDefault();
   if (event.key == 'ArrowUp') {
@@ -39,37 +31,3 @@ const queryButtons = (event: KeyboardEvent): void => {
     constantService.VIEW_DISTANCE += constantService.SPEED / 50;
   }
 }
-
-const times: any[] = [];
-
-const fpsCounter = (): number => {
-  let fps = performance.now();
-  const now = performance.now();
-  while (times.length > 0 && times[0] <= fps - 1000) {
-    times.shift();
-  }
-  times.push(now);
-  return times.length;
-}
-
-
-function main() {
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  cube.render(ctx);
-  
-  if(constantService.DEBUG_MODE){
-    const FPS = fpsCounter();
-    //Print
-    ctx.fillStyle = '#FFF';
-    ctx.fillText(`FPS: ${FPS.toFixed(2)}`, constantService.WIDTH - 100, 20);
-
-    cube.mesh.forEach( (vtx, index) => {
-      ctx.fillText(`[X: ${vtx.X.toFixed(4)}, Y: ${vtx.Y.toFixed(4)} Z: ${vtx.Z.toFixed(4)}]`, 10, 20 + (index++ * 15 ));
-    })
-  }
-  
-  window.requestAnimationFrame(main);
-}
-
-main();
